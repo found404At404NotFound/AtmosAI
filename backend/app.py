60c509b89d47768ab9b31f351729e690ef7e4869
404Found <found404at404notfound@gmail.com>
2025-10-05 17:03:31 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1512,4 +1512,5 @@ def askai():
         'error': 'No matching PDF file found or context could not be processed'
     })
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+
+    app.run(debug=False,host='0.0.0.0')
